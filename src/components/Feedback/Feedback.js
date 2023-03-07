3b06cfe7902c15541405cdc585caf6314424d705
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-07 02:54:20 +0200

    counts on clicks

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import { Component } from "react";
 
-export class Feedback extends React.Component {
+export class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+  
+
     handleGood = () => {
         console.log("good");
+        this.setState(prevState => {
+            return {
+                good: prevState.good + 1,
+            };
+        });
     };
 
     handleNeutral = () => {
         console.log("neutral");
+        this.setState(prevState => {
+            return {
+                neutral: prevState.neutral + 1,
+            };
+        });
     };
 
     handleBad = () => {
         console.log("bad");
+        this.setState(prevState => {
+            return {
+                bad: prevState.bad + 1,
+            };
+        });
     };
 
 
     render() {
         return (
+            <>
             <ul>
                 <li><button type="button" onClick={this.handleGood}>good</button></li>
                 <li><button type="button" onClick={this.handleNeutral}>neutral</button></li>
                 <li><button type="button" onClick={this.handleBad}>bad</button></li>
             </ul>
+
+            <span>Good: {this.state.good}</span>
+            <span>Neutral: {this.state.neutral}</span>
+            <span>Bad: {this.state.bad}</span>
+            </>
         );
     }
 };
