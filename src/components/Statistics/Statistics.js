c59238e9d1854ea766119ff0aa6ce6b2178ad25d
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-07 05:22:18 +0200

    no feedback given when total = 0

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 export function Statistics({good, neutral, bad, total, positivePercentage}) {
     return(
         <>
-            <ul>
-                <li>Good: {good}</li>
-                <li>Neutral: {neutral}</li>
-                <li>Bad: {bad}</li>
-            </ul>
-            <p>Total: {total}</p>
-            <p>Positive feedback: {positivePercentage} %</p>
+            {total === 0 ?
+            (<p>No feedback given</p>) :
+            (<>
+                <ul>
+                    <li>Good: {good}</li>
+                    <li>Neutral: {neutral}</li>
+                    <li>Bad: {bad}</li>
+                </ul>
+                <p>Total: {total}</p>
+                <p>Positive feedback: {positivePercentage} %</p>
+            </>)}
         </>
     );
 };
